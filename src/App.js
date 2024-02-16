b1e248c5c046b50ee4a76ba015e0c3b26d7cc288
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-02-16 22:35:13 +0100

    More Reusability with the Children Prop done

--- src/App.js
+++ src/App.js
@@ -46,9 +46,7 @@ function Steps() {
                         <div className={step > 2 ? "active" : ""}>3</div>
                     </div>
 
-                    <p className="message">
-                        Step {step}: {messages[step - 1]}
-                    </p>
+                    <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
                     <div className="buttons">
                         <Button
@@ -68,6 +66,15 @@ function Steps() {
                     </div>
                 </div>
             )}
+        </div>
+    );
+}
+
+function StepMessage({ step, children }) {
+    return (
+        <div className="message">
+            <h3>Step {step}</h3>
+            {children}
         </div>
     );
 }
